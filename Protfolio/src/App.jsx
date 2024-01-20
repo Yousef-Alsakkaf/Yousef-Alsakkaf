b742ae3200198bb9662ea1fa37edacc5ed049088
Yousef-Alsakkaf <yousefalsakkaf98989@gmail.com>
2024-01-20 14:38:50 +0400

    reset

--- Protfolio/src/App.jsx
+++ Protfolio/src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 function App() {
   
 
@@ -7,6 +11,10 @@ function App() {
     <>
       <div>
         <Navbar></Navbar>
+        <Home></Home>
+        <About></About>
+        <Skills></Skills>
+        <Contact></Contact>
       </div>
     </>
   )
